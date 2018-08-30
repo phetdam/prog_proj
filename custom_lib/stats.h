58a0e4a384387b213a5ab1ee5faca1bba9d26185
Derek <djh458@stern.nyu.edu>
2018-08-29 20:31:23 -0400

    added normalcdf defines to header file

---
+++ custom_lib/stats.h
@@ -0,0 +1,11 @@
+#ifndef STATS_H
+#define STATS_H
+// define constants for normalcdf function
+#define A_1 0.0705230784
+#define A_2 0.0422820123
+#define A_3 0.0092705272
+#define A_4 0.0001520143
+#define A_5 0.0002765672
+#define A_6 0.0000430638
+
+#endif /* STATS_H */
